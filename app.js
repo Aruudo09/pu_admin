955a0562cac2ca4a666913ef9a55af7611443c84
Aruudo09 <audricafabiano@gmail.com>
2025-05-05 09:29:10 +0700

    chat

--- app.js
+++ app.js
@@ -107,3 +107,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// server.listen(3000, '0.0.0.0', () => {
+//   console.log('Server running on http://0.0.0.0:3000');
+// });
